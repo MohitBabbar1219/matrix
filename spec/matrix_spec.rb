8d5de6a847c19b2f02bd4d25a168b73ad7237239
Mohit Babbar <mohitbabbar1219@gmail.com>
2019-01-14 10:30:09 +0530

    add test for validating matrix creation from string with two lines

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -11,4 +11,9 @@ describe 'matrix' do
     expected_row = [3, 4]
     expect(StringMatrix.new("1 2\n3 4").get_row(row_ind)).to eq expected_row
   end
+  it 'should return [[1, 2], [10, 20]] for string: "1 2\n3 4"' do
+    row_ind = 1
+    expected_row = [3, 4]
+    expect(StringMatrix.new("1 2\n3 4").get_row(row_ind)).to eq expected_row
+  end
 end
